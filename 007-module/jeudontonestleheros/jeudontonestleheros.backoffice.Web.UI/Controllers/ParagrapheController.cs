be556115ca8903690b61228cb3875b289ad3c6c6
evan-boissonnot <commercial@boissonnot.Fr>
2018-11-22 23:54:53 +0100

    fin module 7

---
+++ 007-module/jeudontonestleheros/jeudontonestleheros.backoffice.Web.UI/Controllers/ParagrapheController.cs
@@ -0,0 +1,25 @@
+﻿using jeudontonestleheros.Core.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jeudontonestleheros.backoffice.Web.UI.Controllers
+{
+    public class ParagrapheController : Controller
+    {
+        #region Méthodes publiques
+        public ActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Paragraphe paragraphe)
+        {
+            return this.View();
+        }
+        #endregion
+    }
+}
