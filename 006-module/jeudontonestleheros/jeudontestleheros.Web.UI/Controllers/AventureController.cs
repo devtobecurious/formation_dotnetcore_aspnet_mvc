e2dc5d539d18636f888149f13e2039fe34eba4c2
evan-boissonnot <commercial@boissonnot.Fr>
2018-11-22 18:19:12 +0100

    demarrage module 6

---
+++ 006-module/jeudontonestleheros/jeudontestleheros.Web.UI/Controllers/AventureController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jeudontestleheros.Web.UI.Controllers
+{
+    public class AventureController : Controller
+    {
+        public ActionResult Index()
+        {
+            this.ViewBag.MonTitre = "Aventures";
+            this.ViewBag.MonTableau = new int[] { 1, 2, 3, 4, 5 };
+
+            return View();
+        }
+    }
+}
