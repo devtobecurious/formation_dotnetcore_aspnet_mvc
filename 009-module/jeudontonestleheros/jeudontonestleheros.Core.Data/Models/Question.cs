573ca8b0f39e6fe087c625155749fe2ee868d977
evan-boissonnot <commercial@boissonnot.Fr>
2018-11-26 22:57:19 +0100

    initialisation module9

---
+++ 009-module/jeudontonestleheros/jeudontonestleheros.Core.Data/Models/Question.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jeudontonestleheros.Core.Data.Models
+{
+    public class Question
+    {
+        #region Propriétés
+        /// <summary>
+        /// Id de l'instance
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Titre de la question
+        /// </summary>
+        public string Titre { get; set; }
+
+        /// <summary>
+        /// Liste des réponses possibles
+        /// </summary>
+        public List<Reponse> MesReponses { get; set; }
+        #endregion
+    }
+}
