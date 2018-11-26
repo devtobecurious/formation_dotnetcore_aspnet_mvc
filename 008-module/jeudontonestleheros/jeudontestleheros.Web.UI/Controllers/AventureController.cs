7ea2b82e3c49a941a04fb1526ac9f1efc5a5310f
evan-boissonnot <commercial@boissonnot.Fr>
2018-11-26 21:18:56 +0100

    demarrage module8

---
+++ 008-module/jeudontonestleheros/jeudontestleheros.Web.UI/Controllers/AventureController.cs
@@ -0,0 +1,33 @@
+﻿using jeudontestleheros.Web.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jeudontestleheros.Web.UI.Controllers
+{
+    public class AventureController : Controller
+    {
+        public ActionResult Index()
+        {
+            this.ViewBag.MonTitre = "Aventures";
+
+            List<Aventure> maList = new List<Aventure>();
+
+            maList.Add(new Aventure()
+            {
+                Id = 1,
+                Titre = "Ma première aventure"
+            });
+
+            maList.Add(new Aventure()
+            {
+                Id = 2,
+                Titre = "Ma seconde aventure"
+            });
+
+            return View(maList);
+        }
+    }
+}
