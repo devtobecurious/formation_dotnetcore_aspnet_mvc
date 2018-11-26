d850ece0e85d3691a66e48d9b410c394938a4c79
evan-boissonnot <commercial@boissonnot.Fr>
2018-11-26 22:54:02 +0100

    fin module 8

--- 008-module/jeudontonestleheros/jeudontonestleheros.backoffice.Web.UI/Controllers/ParagrapheController.cs
+++ 008-module/jeudontonestleheros/jeudontonestleheros.backoffice.Web.UI/Controllers/ParagrapheController.cs
@@ -9,7 +9,21 @@ namespace jeudontonestleheros.backoffice.Web.UI.Controllers
 {
     public class ParagrapheController : Controller
     {
+        #region A supprimer après entities
+        private List<Paragraphe> _maList = new List<Paragraphe>()
+        {
+            new Paragraphe() { Id = 1, Numero = 1, Titre = "Titre 1"},
+            new Paragraphe() { Id = 2, Numero = 10, Titre = "Titre 2"},
+            new Paragraphe() { Id = 5, Numero = 14, Titre = "Titre 3"}
+        };
+        #endregion
+
         #region Méthodes publiques
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
         public ActionResult Create()
         {
             return this.View();
@@ -19,6 +33,23 @@ namespace jeudontonestleheros.backoffice.Web.UI.Controllers
         public ActionResult Create(Paragraphe paragraphe)
         {
             return this.View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Paragraphe paragraphe = null;
+
+            #region A changer avec l'appel en base de données / entities
+            paragraphe = _maList.First(item => item.Id == id);
+            #endregion
+
+            return this.View(paragraphe);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Paragraphe paragraphe)
+        {
+            return this.View(paragraphe);
         }
         #endregion
     }
