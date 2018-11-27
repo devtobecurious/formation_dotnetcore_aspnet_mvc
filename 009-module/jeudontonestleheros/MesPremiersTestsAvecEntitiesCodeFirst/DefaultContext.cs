371094137cd9cf733f51c89edb0605cadad84b76
evan-boissonnot <commercial@boissonnot.Fr>
2018-11-27 22:41:58 +0100

    fin module 9

---
+++ 009-module/jeudontonestleheros/MesPremiersTestsAvecEntitiesCodeFirst/DefaultContext.cs
@@ -0,0 +1,22 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MesPremiersTestsAvecEntitiesCodeFirst
+{
+    public class DefaultContext : DbContext
+    {
+        public DefaultContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected DefaultContext()
+        {
+        }
+
+        public DbSet<Droide> Droides { get; set; }
+    }
+
+}
