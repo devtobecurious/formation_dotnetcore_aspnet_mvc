53479817fff97e8e3d73b2aebcbfda58694e91cb
evan-boissonnot <commercial@boissonnot.Fr>
2018-11-27 23:10:41 +0100

    initialisation module 10

---
+++ 010-module/jeudontonestleheros/jeudontonestleheros.Core.Data/Models/Reponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jeudontonestleheros.Core.Data.Models
+{
+    public class Reponse
+    {
+        #region Propriétés
+        /// <summary>
+        /// Id de la réponse
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Proposition de réponse
+        /// </summary>
+        public string Description { get; set; }
+        #endregion
+    }
+}
---
+++ 010-module/jeudontonestleheros/jeudontonestleheros.backoffice.Web.UI/Controllers/ParagrapheController.cs
@@ -0,0 +1,56 @@
+﻿using jeudontonestleheros.Core.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jeudontonestleheros.backoffice.Web.UI.Controllers
+{
+    public class ParagrapheController : Controller
+    {
+        #region A supprimer après entities
+        private List<Paragraphe> _maList = new List<Paragraphe>()
+        {
+            new Paragraphe() { Id = 1, Numero = 1, Titre = "Titre 1"},
+            new Paragraphe() { Id = 2, Numero = 10, Titre = "Titre 2"},
+            new Paragraphe() { Id = 5, Numero = 14, Titre = "Titre 3"}
+        };
+        #endregion
+
+        #region Méthodes publiques
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        public ActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Paragraphe paragraphe)
+        {
+            return this.View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Paragraphe paragraphe = null;
+
+            #region A changer avec l'appel en base de données / entities
+            paragraphe = _maList.First(item => item.Id == id);
+            #endregion
+
+            return this.View(paragraphe);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Paragraphe paragraphe)
+        {
+            return this.View(paragraphe);
+        }
+        #endregion
+    }
+}
