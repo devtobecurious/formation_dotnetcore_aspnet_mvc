bf50369a302d2e160aded7a67060726eb663d5e6
evan-boissonnot <commercial@boissonnot.Fr>
2018-12-05 18:40:51 +0100

    demarrage module 11

---
+++ 011-module/jeudontonestleheros/jeudontonestleheros.Core.Data/Models/Paragraphe.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace jeudontonestleheros.Core.Data.Models
+{
+    [Table("Paragraphe")]
+    public class Paragraphe
+    {
+        #region Propriétés
+        /// <summary>
+        /// Id venant de la base
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Numéro à afficher pour le jeu
+        /// </summary>
+        public int Numero { get; set; }
+
+        /// <summary>
+        /// C'est le titre du paragraphe
+        /// </summary>
+        public string Titre { get; set; }
+
+        /// <summary>
+        /// Description du paragraphe
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Question du paragraphe
+        /// </summary>
+        [NotMapped]
+        public Question MaQuestion { get; set; }
+        #endregion
+    }
+}
