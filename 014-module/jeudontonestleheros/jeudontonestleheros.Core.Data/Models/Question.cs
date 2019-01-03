100c22ee64c05ddfd0477e0100c443359c31641d
evan-boissonnot <commercial@boissonnot.Fr>
2019-01-03 23:48:07 +0100

    demarrage module14

---
+++ 014-module/jeudontonestleheros/jeudontonestleheros.Core.Data/Models/Question.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace jeudontonestleheros.Core.Data.Models
+{
+    [Table("Question")]
+    public class Question
+    {
+        #region Propriétés
+        /// <summary>
+        /// Id de l'instance
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Titre de la question
+        /// </summary>
+        public string Titre { get; set; }
+
+        public int ParagrapheId { get; set; }
+
+        /// <summary>
+        /// Liste des réponses possibles
+        /// </summary>
+        public List<Reponse> MesReponses { get; set; }
+        #endregion
+    }
+}
