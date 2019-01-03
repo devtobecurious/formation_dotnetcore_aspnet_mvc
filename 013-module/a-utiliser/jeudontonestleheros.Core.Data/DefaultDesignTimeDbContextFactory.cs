b88b51f103c5a6c6602018bf5d83f2c249dc6a8f
evan-boissonnot <commercial@boissonnot.Fr>
2019-01-03 22:22:05 +0100

    preparation module13

---
+++ 013-module/a-utiliser/jeudontonestleheros.Core.Data/DefaultDesignTimeDbContextFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace jeudontonestleheros.Core.Data
+{
+    public class DefaultDesignTimeDbContextFactory : IDesignTimeDbContextFactory<DefaultContext>
+    {
+        public DefaultContext CreateDbContext(string[] args)
+        {
+            string path = Directory.GetCurrentDirectory();
+
+            var builder = new ConfigurationBuilder()
+                               .SetBasePath(path)
+                               .AddJsonFile("appsettings.json");
+
+
+            var config = builder.Build();
+
+            var connectionString = config.GetConnectionString("DefaultContext");
+
+            DbContextOptionsBuilder<DefaultContext> optionBuilder = new DbContextOptionsBuilder<DefaultContext>();
+            optionBuilder.UseSqlServer(connectionString);
+
+            return new DefaultContext(optionBuilder.Options);
+        }
+    }
+}
---
+++ 013-module/jeudontonestleheros/jeudontonestleheros.Core.Data/Models/Question.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace jeudontonestleheros.Core.Data.Models
+{
+    [Table("Question")]
+    public class Question
+    {
+        #region Propriétés
+        /// <summary>
+        /// Id de l'instance
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Titre de la question
+        /// </summary>
+        public string Titre { get; set; }
+
+        /// <summary>
+        /// Liste des réponses possibles
+        /// </summary>
+        [NotMapped]
+        public List<Reponse> MesReponses { get; set; }
+        #endregion
+    }
+}
