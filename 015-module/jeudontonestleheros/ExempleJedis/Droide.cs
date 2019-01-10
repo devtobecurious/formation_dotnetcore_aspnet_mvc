e6bad62031e3f5770ed0d51c7a05186ce41197ee
evan-boissonnot <commercial@boissonnot.Fr>
2019-01-10 21:46:42 +0100

    fin module15

---
+++ 015-module/jeudontonestleheros/ExempleJedis/Droide.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExempleJedis
+{
+    public class Droide
+    {
+        public int PointsDeVie { get; set; }
+    }
+}
